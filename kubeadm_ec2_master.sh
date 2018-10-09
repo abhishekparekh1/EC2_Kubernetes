ed219a4f914d545e72d84468e3ba5bde151d13b0
root <root@ip-172-31-20-248.us-east-2.compute.internal>
2018-10-09 10:59:35 +0000

    Kubernetes Script

---
+++ kubeadm_ec2_master.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+sudo apt-get update && sudo apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+apt-mark hold kubelet kubeadm kubectl
+sudo apt-get update
+sudo apt-get install     apt-transport-https     ca-certificates     curl     software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+$(lsb_release -cs) \
+stable"
+sudo apt-get update
+sudo apt-get install docker-ce -y
+kubeadm init --pod-network-cidr=192.168.0.0/16
+echo "export KUBECONFIG=\"/etc/kubernetes/admin.conf\"" >> ~/.bashrc
+echo "alias k='kubectl'" >> ~/.bashrc
+source ~/.bashrc
+kubectl apply -f https://docs.projectcalico.org/v3.1/getting-started/kubernetes/installation/hosted/rbac-kdd.yaml
+kubectl apply -f https://docs.projectcalico.org/v3.1/getting-started/kubernetes/installation/hosted/kubernetes-datastore/calico-networking/1.7/calico.yaml
+kubectl taint nodes --all node-role.kubernetes.io/master-
+wget https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/alternative/kubernetes-dashboard.yaml
+echo '  type: NodePort' >> kubernetes-dashboard.yaml 
+kubectl apply -f kubernetes-dashboard.yaml 
+kubectl create -f https://gist.github.com/ofirmakmal/05ada26c02fd09f335580ba851252cfb/raw/ab522abda9d6f6e38427ce1440410cf4bb1f6d56/dashboard-admin.yaml
+
