e2d411434cdb596bb9858707541e52d35a3b280b
root <root@ip-172-31-16-169.us-east-2.compute.internal>
2018-10-10 19:56:33 +0000

    Path Correction

--- master_script.sh
+++ master_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo su <<EOF
 git clone -b apache-server https://github.com/abhishekparekh1/EC2_Kubernetes.git
-bash apache-server.sh
+bash EC2_Kubernetes/apache-server.sh
 source EC2_Kubernetes/kubeadm_ec2_master.sh
 cp EC2_Kubernetes/kubeadm_join.sh /var/www/html/
 tail -n 2 /var/www/html/jointoken.html >> /var/www/html/kubeadm_join.sh
