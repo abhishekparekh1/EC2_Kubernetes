f5b9ddc5c5e0565a78c7aef94f52552145e1334b
root <root@ip-172-31-34-90.us-east-2.compute.internal>
2018-10-10 20:13:59 +0000

    slave_script cp

--- master_script.sh
+++ master_script.sh
@@ -5,5 +5,5 @@ bash EC2_Kubernetes/apache-server.sh
 source EC2_Kubernetes/kubeadm_ec2_master.sh
 cp EC2_Kubernetes/kubeadm_join.sh /var/www/html/
 tail -n 2 /var/www/html/jointoken.html >> /var/www/html/kubeadm_join.sh
-cp /home/ubuntu/EC2_Kubernetes/slave_script.sh /var/www/html/
+cp EC2_Kubernetes/slave_script.sh /var/www/html/
 EOF
