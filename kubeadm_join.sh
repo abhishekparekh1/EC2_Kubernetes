7fa204db5205ed409cc059ac4a3eab87e4816f02
root <root@ip-172-31-31-144.us-east-2.compute.internal>
2018-10-10 14:53:38 +0000

    kubeadm join

--- kubeadm_join.sh
+++ kubeadm_join.sh
@@ -1,3 +1,4 @@
+#Kubeadm join script
 apt-get update
 apt-get update && apt-get install -y apt-transport-https curl
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
@@ -16,16 +17,7 @@ $(lsb_release -cs) \
 stable"
 sudo apt-get update
 sudo apt-get install docker-ce -y
-kubeadm init --pod-network-cidr=192.168.0.0/16
 export KUBECONFIG=/etc/kubernetes/admin.conf
-kubectl apply -f https://docs.projectcalico.org/v3.1/getting-started/kubernetes/installation/hosted/rbac-kdd.yaml
-kubectl apply -f https://docs.projectcalico.org/v3.1/getting-started/kubernetes/installation/hosted/kubernetes-datastore/calico-networking/1.7/calico.yaml
-kubectl taint nodes --all node-role.kubernetes.io/master-
-wget https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/alternative/kubernetes-dashboard.yaml
-echo '  type: NodePort' >> kubernetes-dashboard.yaml 
-kubectl apply -f kubernetes-dashboard.yaml 
-kubectl create -f https://gist.github.com/ofirmakmal/05ada26c02fd09f335580ba851252cfb/raw/ab522abda9d6f6e38427ce1440410cf4bb1f6d56/dashboard-admin.yaml
 alias k='kubectl' >> ~/.bashrc
 source ~/.bashrc
-kubectl get svc -n kube-system
-
+#Also run the output of the kbeadm init command on all the subsequent nodes
