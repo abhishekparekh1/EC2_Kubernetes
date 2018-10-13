1ea87c20df10aadd09bb4c5e2be4a2f01d99083c
root <root@ip-172-31-31-53.us-east-2.compute.internal>
2018-10-13 18:32:34 +0000

    Master-Untainted

--- kubeadm_ec2_master.sh
+++ kubeadm_ec2_master.sh
@@ -20,7 +20,6 @@ kubeadm init --pod-network-cidr=192.168.0.0/16 > /var/www/html/jointoken.html
 export KUBECONFIG=/etc/kubernetes/admin.conf
 kubectl apply -f https://docs.projectcalico.org/v3.1/getting-started/kubernetes/installation/hosted/rbac-kdd.yaml
 kubectl apply -f https://docs.projectcalico.org/v3.1/getting-started/kubernetes/installation/hosted/kubernetes-datastore/calico-networking/1.7/calico.yaml
-kubectl taint nodes --all node-role.kubernetes.io/master-
 git clone https://github.com/kubernetes/heapster.git
 kubectl create -f heapster/deploy/kube-config/influxdb/
 kubectl create -f heapster/deploy/kube-config/rbac/heapster-rbac.yaml
