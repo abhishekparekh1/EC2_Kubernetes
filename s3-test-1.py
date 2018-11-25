053d93408f932a07e77a0abcabac6e8ff42a430c
abhishekparekh1 <abhishekparekh1@gmail.com>
2018-11-25 10:16:47 -0500

    Update s3-test-1.py

--- s3-test-1.py
+++ s3-test-1.py
@@ -1,5 +1,5 @@
 import boto3
 
 s3 = boto3.resource('s3')
-s3.create_bucket(Bucket='my-unique-fucking1-test-bucket',CreateBucketConfiguration={
+s3.create_bucket(Bucket='my-unique-f1-test-bucket',CreateBucketConfiguration={
     'LocationConstraint': 'us-east-2'})
