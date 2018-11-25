518bf943d1edc395fcbc3bc2055aa3c4d850ccb2
abhishekparekh1 <abhishekparekh1@gmail.com>
2018-11-25 10:17:10 -0500

    Update s3-test-2.py

--- s3-test-2.py
+++ s3-test-2.py
@@ -1,4 +1,4 @@
 import boto3
 
 s3 = boto3.resource('s3')
-s3.Object('my-unique-fucking1-test-bucket', 'hello.txt').put(Body=open('/home/ubuntu/tmp/hello.txt', 'rb'))
+s3.Object('my-unique-f1-test-bucket', 'hello.txt').put(Body=open('/home/ubuntu/tmp/hello.txt', 'rb'))
